08fa58d6804771a580a88042242ece5d5af8449b
Shilin Victor <chrono.monochrome@gmail.com>
2015-12-05 13:59:38 +0000

    Change zram size and add minfree script

--- scripts/initd_install.sh
+++ scripts/initd_install.sh
@@ -17,5 +17,9 @@ mount -o remount /system
 
 install_if_not_exists 00autoload
 install_if_not_exists 10dynamic
+
 cp /tmp/60zram /system/etc/init.d/60zram
 chmod 755 /system/etc/init.d/60zram
+
+cp /tmp/20minfree /system/etc/init.d/20minfree
+chmod 755 /system/etc/init.d/20minfree
