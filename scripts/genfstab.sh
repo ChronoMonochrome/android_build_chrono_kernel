494b3610b81b9cae575138104381b6fe5e93b635
Shilin Victor <chrono.monochrome@gmail.com>
2016-02-12 11:48:31 +0700

    Revert "Symlink /cache to /efs/cache"
    This reverts commit 4c27f9fd2c2012b968457f8cc0e4c453acc6be67.

--- scripts/genfstab.sh
+++ scripts/genfstab.sh
@@ -3,7 +3,7 @@
 set -x
 
 DATA=/dev/block/mmcblk0p5
-#CACHE=/dev/block/mmcblk0p4
+CACHE=/dev/block/mmcblk0p4
 SYSTEM=/dev/block/mmcblk0p3
 
 DIR=/tmp
@@ -13,8 +13,8 @@ dd if=$DATA of=/tmp/tmp count=1 bs=1036
 is_data_f2fs=$(grep -c $'\x10\x20\xF5\xF2' /tmp/tmp)  
 dd if=$SYSTEM of=/tmp/tmp count=1 bs=1036
 is_system_f2fs=$(grep -c $'\x10\x20\xF5\xF2' /tmp/tmp) 
-#dd if=$CACHE of=/tmp/tmp count=1 bs=1036
-#is_cache_f2fs=$(grep -c $'\x10\x20\xF5\xF2' /tmp/tmp) 
+dd if=$CACHE of=/tmp/tmp count=1 bs=1036
+is_cache_f2fs=$(grep -c $'\x10\x20\xF5\xF2' /tmp/tmp) 
 
 cat $DIR/fstab1 > $FSTAB
 cat $DIR/fstab1 >> /tmp/kernel_log.txt
@@ -30,13 +30,13 @@ fi
 cat $DIR/fstab2 >> $FSTAB
 cat $DIR/fstab2 >> /tmp/kernel_log.txt
 
-#if [ $is_cache_f2fs == 0 ] ; then
-#	cat $DIR/cache_ext4 >> $FSTAB ;
-#	cat $DIR/cache_ext4 >> /tmp/kernel_log.txt
-#else
-#	cat $DIR/cache_f2fs >> $FSTAB ;
-#	cat $DIR/cache_f2fs >> /tmp/kernel_log.txt
-#fi
+if [ $is_cache_f2fs == 0 ] ; then
+	cat $DIR/cache_ext4 >> $FSTAB ;
+	cat $DIR/cache_ext4 >> /tmp/kernel_log.txt
+else
+	cat $DIR/cache_f2fs >> $FSTAB ;
+	cat $DIR/cache_f2fs >> /tmp/kernel_log.txt
+fi
 
 cat $DIR/fstab3 >> $FSTAB
 cat $DIR/fstab3 >> /tmp/kernel_log.txt
