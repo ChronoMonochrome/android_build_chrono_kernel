a8c194edccbb10a1c32a11e17b90c38f3d7958b6
Shilin Victor <chrono.monochrome@gmail.com>
2016-04-30 23:01:50 +0700

    genfstab: add marshmallow support

--- scripts/genfstab.sh
+++ scripts/genfstab.sh
@@ -2,12 +2,40 @@
 
 set -x
 
+# genfstab config file
+FSTAB_SPECS="/ramdisk/fstab_specs.txt"
+
+
+# defaults
+FSTAB_VER="1.0"
 DATA=/dev/block/mmcblk0p5
 CACHE=/dev/block/mmcblk0p4
 SYSTEM=/dev/block/mmcblk0p3
 
+VERSION_LINE=$(cat /system/build.prop | grep "ro.build.version.release" | cut -d "=" -f2)
+VX=$(echo $VERSION_LINE | cut -d "." -f1)
+VY=$(echo $VERSION_LINE | cut -d "." -f2)
+
+if [ $VX == 6 ]; then
+	FSTAB_VER="2.0"
+fi
+
+if ! test -f $FSTAB_SPECS ; then
+	touch $FSTAB_SPECS
+	echo "ver="$FSTAB_VER >> $FSTAB_SPECS
+	echo "data="$DATA >> $FSTAB_SPECS
+	echo "system="$SYSTEM >> $FSTAB_SPECS
+	echo "cache="$CACHE >> $FSTAB_SPECS
+else
+	DATA=$(cat $FSTAB_SPECS | grep "data=" | cut -d "=" -f2)
+	SYSTEM=$(cat $FSTAB_SPECS | grep "system=" | cut -d "=" -f2)
+	CACHE=$(cat $FSTAB_SPECS | grep "cache=" | cut -d "=" -f2)
+fi
+
+
 DIR=/tmp
 FSTAB=/ramdisk/fstab
+FSTAB_SWAP=/ramdisk/fstab_swap
 
 dd if=$DATA of=/tmp/tmp count=1 bs=1036
 is_data_f2fs=$(grep -c $'\x10\x20\xF5\xF2' /tmp/tmp)  
@@ -49,5 +77,11 @@ else
 	cat $DIR/data_f2fs >> /tmp/kernel_log.txt
 fi
 	
-cat $DIR/fstab4 >> $FSTAB
-cat $DIR/fstab4 >> /tmp/kernel_log.txt
+
+cat $FSTAB > $FSTAB_SWAP
+
+cat $DIR/fstab4_v$FSTAB_VER >> $FSTAB
+cat $DIR/fstab4_v$FSTAB_VER >> /tmp/kernel_log.txt
+
+cat $DIR/fstab4_v$FSTAB_VER"_swap" >> $FSTAB_SWAP
+cat $DIR/fstab4_v$FSTAB_VER"_swap" >> /tmp/kernel_log.txt
