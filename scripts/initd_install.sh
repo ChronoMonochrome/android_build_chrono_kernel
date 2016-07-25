4688c983d4084f331eb41dcbd6a1c1eb27deb605
Shilin Victor <chrono.monochrome@gmail.com>
2016-07-26 05:51:05 +0700

    Install scripts to ramdisk/init.d

--- scripts/initd_install.sh
+++ scripts/initd_install.sh
@@ -1,10 +1,12 @@
 #!/sbin/sh
 
+SCRIPTS_DIR=/ramdisk/init.d
+
 set -x
 
 install_if_not_exists()
 {    
-       script=/system/etc/init.d/$1
+       script=$SCRIPTS_DIR/$1
        if test -f $script ; then
             echo "$1 exists" ;
        else
@@ -18,8 +20,17 @@ mount -o remount /system
 install_if_not_exists 00autoload
 install_if_not_exists 10dynamic
 
-cp /tmp/60zram /system/etc/init.d/60zram
-chmod 755 /system/etc/init.d/60zram
+cp /tmp/60zram $SCRIPTS_DIR/60zram
+chmod 755  $SCRIPTS_DIR/60zram
 
-cp /tmp/20minfree /system/etc/init.d/20minfree
-chmod 755 /system/etc/init.d/20minfree
+cp /tmp/20minfree  $SCRIPTS_DIR/20minfree
+chmod 755 $SCRIPTS_DIR/20minfree
+
+if test -f /system/etc/init.d/60zram ; then
+	rm /system/etc/init.d/60zram
+fi
+
+if test -f /system/etc/init.d/20minfree ; then
+	rm /system/etc/init.d/20minfree
+fi
+
