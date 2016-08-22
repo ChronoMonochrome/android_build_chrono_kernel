84e8818c80b73dbfd0762e8f134543e5eed8401e
Shilin Victor <chrono.monochrome@gmail.com>
2016-08-23 01:02:43 +0700

    recovery_install: fix derp

--- scripts/recovery_install.sh
+++ scripts/recovery_install.sh
@@ -33,23 +33,6 @@ fi
 
 mount /system
 
-DEVICE=$(cat /system/build.prop | grep "ro.product.device=" | cut -d "=" -f2)
-
-if [ "$DEVICE" == "GT-I8160" ] ; then
-	DEVICE=codina
-fi
-
-if [ "$DEVICE" == "GT-I8160P" ] ; then
-	DEVICE=codinap
-fi
-
-DEF_PROP="recovery_default.prop"
-dev_files="/tmp/"$DEVICE
-
-mv -f /ramdisk/$DEF_PROP /ramdisk/$DEF_PROP".bak"
-cp $dev_files/$DEF_PROP /ramdisk
-
-
 if test -f /ramdisk/recovery.cpio.gz ; then
 	exit
 fi
