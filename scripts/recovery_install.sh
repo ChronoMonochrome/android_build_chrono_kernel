560683e7cfd154d97d7cb2d7443b714c06d04005
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-13 16:35:49 +0300

    Revert "efs recovery install test"
    This reverts commit dada0e103d2d89877bc52992afdc5a19f39baeb2.

--- scripts/recovery_install.sh
+++ scripts/recovery_install.sh
@@ -22,35 +22,26 @@ cd $cur_dir
 cd /tmp
 
 if [ "$(busybox dd if=/dev/block/mmcblk0p15 skip=8388608 bs=1 count=3 | busybox grep -c $'\x1f\x8b\x08' )" == "0" ] ; then
-	# gzip -9 recovery.cpio
-	# dd if=/tmp/recovery.cpio.gz of=/dev/block/mmcblk0p15 bs=524288 seek=16
-	dd if=/dev/zero of=/dev/block/mmcblk0p15 bs=524288
+	gzip -9 recovery.cpio
+
+	dd if=/tmp/recovery.cpio.gz of=/dev/block/mmcblk0p15 bs=524288 seek=16
+
 fi
 
 
-mount /efs
+# install default.prop
+
+mount /system
 
 if test -f /ramdisk/recovery.cpio.gz ; then
-	rm -f /ramdisk/recovery.cpio.gz
+	exit
 fi
 
 if  test -f /ramdisk/recovery.cpio ; then
-	rm -f /ramdisk/recovery.cpio
-fi
-
-if test -f /efs/recovery.cpio.gz ; then
-	ln -sf /efs/recovery.cpio.gz /ramdisk/
 	exit
 fi
 
-if  test -f /efs/recovery.cpio ; then
-	ln -sf /efs/recovery.cpio /ramdisk/
-	exit
-fi
 
-gzip -9 recovery.cpio
-
-cp /tmp/recovery.cpio.gz /efs/recovery.cpio.gz
-ln -sf /efs/recovery.cpio.gz /ramdisk/
+cp /tmp/recovery.cpio.gz /ramdisk/recovery.cpio.gz
 
 rm /tmp/recovery.cpio.gz
