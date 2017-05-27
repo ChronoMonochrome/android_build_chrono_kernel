58cec75f8f4d6a562ea352efeb8d4652880b0262
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-27 19:01:44 +0300

    Add parted to TWRP

--- scripts/recovery_install.sh
+++ scripts/recovery_install.sh
@@ -21,7 +21,7 @@ md5="$(busybox dd if=/dev/block/mmcblk0p15 skip=8388608 bs=1 count=10 | md5sum |
 #is_gzip="$(busybox dd if=/dev/block/mmcblk0p15 skip=8388608 bs=1 count=3 | busybox grep -c $'\x1f\x8b\x08' )"
 
 #if [ "$md5" == "4f05c2785cbcd1f144c2db58ea33f92d" ] || [ "$is_gzip" == "0" ] ; then
-if [ "$md5" != "1909f12fcd29d9bf15af95d83a7d223a" ] ; then
+if [ "$md5" != "578a5125b88211804b59aef2c66cc3b5" ] ; then
 	dd if=/tmp/recovery.cpio.gz of=/dev/block/mmcblk0p15 bs=524288 seek=16
 fi
 
