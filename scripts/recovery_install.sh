fb197ea42d1fb1f1b370f66d3a6fc646cd5eebf2
Shilin Victor <chrono.monochrome@gmail.com>
2018-01-14 06:47:06 +0300

    Revert "Revert "Update recovery""
    This reverts commit 2c3a1a9efacdc46722de60a7bf1d9e806323acdb.

--- scripts/recovery_install.sh
+++ scripts/recovery_install.sh
@@ -18,25 +18,10 @@ fi
 ##### Unpack ramdisk.7z #####
 
 md5="$(busybox dd if=/dev/block/mmcblk0p15 skip=8388608 bs=1 count=10 | md5sum | cut -d ' ' -f1 )"
-#is_gzip="$(busybox dd if=/dev/block/mmcblk0p15 skip=8388608 bs=1 count=3 | busybox grep -c $'\x1f\x8b\x08' )"
 
-#if [ "$md5" == "4f05c2785cbcd1f144c2db58ea33f92d" ] || [ "$is_gzip" == "0" ] ; then
-if [ "$md5" != "ce45791aa83635990cfe7869ed83dc78" ] ; then
+if [ "$md5" != "a2d61d4da7c678ba411fe69d94429da5" ] ; then
 	dd if=/tmp/recovery.img of=/dev/block/mmcblk0p15 bs=524288 seek=16
 fi
-
-#if [ "$is_gzip" == "1" ]; then
-#	busybox dd if=/dev/block/mmcblk0p15 bs=524288 skip=16 | busybox gzip -d > /recovery.cpio 2> /ramdisk/recovery.check
-#	busybox rm /recovery.cpio
-#	if [ "$(busybox cat /ramdisk/recovery.check | busybox grep -c 'gzip')" != 0 ] ; then
-#		busybox echo "recovery image appears to be corrupted, forcing installation"
-#		cp /tmp/recovery.cpio /
-#		cd /
-#		gzip -9 recovery.cpio
-#		dd if=/recovery.cpio.gz of=/dev/block/mmcblk0p15 bs=524288 seek=16
-#	fi
-#fi
-
 
 # install default.prop
 
