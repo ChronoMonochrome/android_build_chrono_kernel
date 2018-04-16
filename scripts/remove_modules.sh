dd7b22b3104ce02cb5a293ba41a85850d60437f6
Shilin Victor <chrono.monochrome@gmail.com>
2018-04-16 09:46:30 +0300

    scripts: remove also system modules on install

--- scripts/remove_modules.sh
+++ scripts/remove_modules.sh
@@ -1,6 +1,8 @@
 #!/sbin/sh
 
 set -x
+rm -f /ramdisk/modules/autoload/*.ko
+rm -f /ramdisk/modules/*.ko
 rm -fr /system/lib/modules/
 mkdir -p /system/lib/modules/autoload
 chmod 755 /system/lib/modules/autoload
